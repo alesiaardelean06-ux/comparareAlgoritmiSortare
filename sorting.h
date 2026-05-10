eb95018a607b57f36e876d4ba8a38fb1f2db99fc
alesiaardelean06-ux <alesia.ardelean06@e-uvt.ro>
2026-05-10 17:51:06 +0300

    Add time limit parameters to sorting functions

--- sorting.h
+++ sorting.h
@@ -1,13 +1,18 @@
 #ifndef SORTING_H
 #define SORTING_H
 #include <stddef.h>
-void insertion_sort(int *v, size_t n);
-void selection_sort(int *v, size_t n);
-void bubble_sort(int *v, size_t n);
-void merge_sort(int *v, size_t s, size_t d);
-void quicksort(int *v, size_t s, size_t d);
-void shell_sort(int *v, size_t n);
-void count_sort(int *v, size_t n);
-void cocktail_shaker_sort(int *v, size_t n);
-void gnome_sort(int *v, size_t n);
-#endif 
+#include <time.h>
+#define TIME_LIMIT_QUADRATIC_SECONDS 60.0 //Bubble, Selection, Insertion, Gnome, Cocktail
+#define TIME_LIMIT_GENERAL_SECONDS 120.0 //Merge, Shell, Count 
+#define TIME_LIMIT_QUICK_SECONDS 10.0 // Quick Sort 
+int time_limit_exceeded(clock_t start, double time_limit_seconds);
+int insertion_sort(int *v, size_t n, clock_t start, double time_limit);
+int selection_sort(int *v, size_t n, clock_t start, double time_limit);
+int bubble_sort(int *v, size_t n, clock_t start, double time_limit);
+int merge_sort(int *v, size_t s, size_t d, clock_t start, double time_limit);
+int quicksort(int *v, size_t s, size_t d, clock_t start, double time_limit);
+int shell_sort(int *v, size_t n, clock_t start, double time_limit);
+int count_sort(int *v, size_t n, clock_t start, double time_limit);
+int cocktail_shaker_sort(int *v, size_t n, clock_t start, double time_limit);
+int gnome_sort(int *v, size_t n, clock_t start, double time_limit);
+#endif
